62505c70427511f3b7b885a36088657d992a5049
sadvani06 <bunnimina@gmail.com>
2014-04-30 20:13:06 -0400

    added in box text html and next

--- effect.js
+++ effect.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 $(".box").not("#box1").hide();
 var questions=[
-              {question:"Question1", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:1};
+                          {question:"Question1", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:1};
 			  {question:"Question2", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:2};
 			  {question:"Question3", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:3};
 			  {question:"Question4", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:4};
@@ -12,3 +12,20 @@ var questions=[
 			  {question:"Question9", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:1};
 			  {question:"Question10", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:2};
 			  ]
+			  
+$("#box1").html(function()
+
+
+	var choiceDisplay="<ul>";
+	for var(i=0; i < questions[1].choice.length ; i++){
+	choiceDisplay + "<li><input type='radio'>" + choice[i] + "</li>"	
+	}
+	var choiceDisplay += "</ul>";
+	return choiceDisplay;
+);
+
+$("#nextButton").on('click', function(){
+	if(input === questions[1].correct){
+		ADD MORE CODE HERE
+	}
+})
