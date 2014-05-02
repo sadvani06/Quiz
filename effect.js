52797609628911964b7ee82e2cd6c5ef934db404
sadvani06 <bunnimina@gmail.com>
2014-05-02 09:47:32 -0400

    Corrrected Syntax. Semicolon to comma 3-14

--- effect.js
+++ effect.js
@@ -1,16 +1,16 @@
-// JavaScript Document
+
 $(".box").not("#box1").hide();
 var questions=[
-                          {question:"Question1", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:1};
-			  {question:"Question2", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:2};
-			  {question:"Question3", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:3};
-			  {question:"Question4", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:4};
-			  {question:"Question5", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:1};
-			  {question:"Question6", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:2};
-			  {question:"Question7", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:3};
-			  {question:"Question8", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:4};
-			  {question:"Question9", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:1};
-			  {question:"Question10", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:2};
+                          {question:"Question1", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:1},
+			  {question:"Question2", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:2},
+			  {question:"Question3", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:3},
+			  {question:"Question4", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:4},
+			  {question:"Question5", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:1},
+			  {question:"Question6", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:2},
+			  {question:"Question7", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:3},
+			  {question:"Question8", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:4},
+			  {question:"Question9", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:1},
+			  {question:"Question10", choices:["Answer1", "Answer2", "Answer3", "Answer4"], correct:2}
 			  ]
 			  
 $("#box1").html(function()
